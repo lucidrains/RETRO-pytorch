d37e984198c1334571de4735deb76daca372c260
Phil Wang <lucidrains@gmail.com>
2022-01-17 11:28:58 -0800

    attention

--- retro_pytorch/retro_pytorch.py
+++ retro_pytorch/retro_pytorch.py
@@ -9,6 +9,9 @@ from einops import rearrange
 def exists(val):
     return val is not None
 
+def default(val, d):
+    return val if exists(val) else d
+
 # helper classes
 
 class RMSNorm(nn.Module):
@@ -29,11 +32,75 @@ class RMSNorm(nn.Module):
 
 def FeedForward(dim, mult = 4, dropout = 0.):
     return nn.Sequential(
+        RMSNorm(dim),
         nn.Linear(dim, dim * mult),
         nn.GELU(),
         nn.Dropout(dropout),
         nn.Linear(dim * mult, dim)
     )
+
+class Attention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        *,
+        dim_head = 64,
+        heads = 8,
+        causal = False
+    ):
+        super().__init__()
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        self.causal = causal
+        inner_dim = dim_head * heads
+
+        self.to_q = nn.Linear(dim, inner_dim, bias = False)
+        self.to_kv = nn.Linear(dim, inner_dim * 2, bias = False)
+        self.to_out = nn.Linear(inner_dim, dim)
+
+    def forward(self, x, context = None):
+        device, h, scale = x.device, self.heads, self.scale
+        kv_input = default(context, x)
+
+        q = self.to_q(x)
+        k, v = self.to_kv(kv_input).chunk(2, dim = -1)
+
+        # split heads
+
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), (q, k, v))
+
+        # scale
+
+        q = q * scale
+
+        # derive query key similarities
+
+        sim = einsum('b h i d, b h j d -> b h i j', q, k)
+
+        # masking
+
+        if self.causal:
+            i, j = sim.shape[-2:]
+            causal_mask = torch.ones(i, j, device = device).triu(j - i + 1)
+            mask_value = -torch.finfo(sim.dtype).max
+            sim = sim.masked_fill(causal_mask, mask_value)
+
+        # attention
+
+        attn = sim.softmax(dim = -1)
+
+        # aggregate
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+
+        # merge heads
+
+        out = rearrange(out, 'b h n d -> b n (h d)')
+
+        # combine heads linear out
+
+        return self.to_out(out)
+
 
 # main class
 
@@ -70,32 +137,34 @@ class RETRO(nn.Module):
     ):
         assert not (return_loss and not self.training), 'must be training if returning loss'
 
-        n, device = seq.shape[-1], seq.device
-
-        # use 0 as bos
-
-        seq = F.pad(seq, (1, 0), value = 0)
-
         # if training, derive labels
 
         if return_loss:
             seq, labels = seq[:, :-1], seq[:, 1:]
+
+        # variables
+
+        n, device = seq.shape[-1], seq.device
 
         # embed both sequence and retrieved chunks
 
         embed = self.token_emb(seq)
         retrieved = self.token_emb(retrieved)
 
-        # get positional embedding
+        # get absolute positional embedding
 
         pos_emb = self.pos_emb(torch.arange(n, device = device))
         pos_emb = rearrange(pos_emb, 'n d -> 1 n d')
         embed = embed + pos_emb
 
+        # project to logits
+
         logits = self.to_logits(embed)
 
         if not return_loss:
             return logits
+
+        # cross entropy loss
 
         loss = F.cross_entropy(rearrange(logits, 'b n c -> b c n'), labels)
         return loss
