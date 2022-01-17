af6008637bdbe00bb9f112359827ff3c3c282ec0
Phil Wang <lucidrains@gmail.com>
2022-01-17 09:02:01 -0800

    scaffold

---
+++ retro_pytorch/retro_pytorch.py
@@ -0,0 +1,16 @@
+import torch
+from torch import nn, einsum
+from einops import rearrange
+
+# helper functions
+
+# helper classes
+
+# main class
+
+class RETRO(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return x
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'retro-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license='MIT',
+  description = 'RETRO - Retrieval Enhanced Transformer - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/RETRO-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformers',
+    'attention-mechanism',
+    'retrieval',
+  ],
+  install_requires=[
+    'einops>=0.3',
+    'faiss-gpu',
+    'numpy',
+    'torch>=1.6',
+    'tqdm'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
