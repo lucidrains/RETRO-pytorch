d8349109d9aab831d85221638238864567efcfa7
Phil Wang <lucidrains@gmail.com>
2022-01-23 10:47:23 -0800

    use contextmanager for loading memmap

--- retro_pytorch/data.py
+++ retro_pytorch/data.py
@@ -1,6 +1,14 @@
+from functools import partial
 import numpy as np
 import torch
 from torch.utils.data import Dataset
+from contextlib import contextmanager
+
+@contextmanager
+def memmap(*args, **kwargs):
+    pointer = np.memmap(*args, **kwargs)
+    yield pointer
+    del pointer
 
 # dataset
 
@@ -24,38 +32,40 @@ class RETRODataset(Dataset):
         self.seq_num_chunks = seq_len // chunk_size
         shape = (num_chunks, chunk_size + 1)
 
-        self.chunks = np.memmap(chunk_memmap_path, dtype = np.int32, shape = shape)
-        self.masks = np.memmap(mask_memmap_path, dtype = np.bool, shape = shape)
-        self.continuations = np.memmap(chunk_continuation_memmap_path, dtype = np.int32, shape = (num_chunks,))
-        self.knns = np.memmap(chunk_nn_memmap_path, dtype = np.int32, shape = (num_chunks, num_neighbors))
-        self.seqs = np.memmap(seq_memmap_path, dtype = np.int32, shape = (num_sequences,))
+        self.get_chunks = partial(memmap, chunk_memmap_path, dtype = np.int32, shape = shape)
+        self.get_masks = partial(memmap, mask_memmap_path, dtype = np.bool, shape = shape)
+        self.get_continuations = partial(memmap, chunk_continuation_memmap_path, dtype = np.int32, shape = (num_chunks,))
+        self.get_knns = partial(memmap, chunk_nn_memmap_path, dtype = np.int32, shape = (num_chunks, num_neighbors))
+        self.get_seqs = partial(memmap, seq_memmap_path, dtype = np.int32, shape = (num_sequences,))
 
     def __len__(self):
         return self.num_sequences
 
     def __getitem__(self, ind):
-        begin_chunk_index = self.seqs[ind]
-        chunk_range = slice(begin_chunk_index, (begin_chunk_index + self.seq_num_chunks))
+        with self.get_chunks() as chunks_memmap, self.get_masks() as masks_memmap, self.get_continuations() as continuations_memmap, self.get_knns() as knns_memmap, self.get_seqs() as seqs_memmap:
 
-        chunks = self.chunks[chunk_range]
+            begin_chunk_index = seqs_memmap[ind]
+            chunk_range = slice(begin_chunk_index, (begin_chunk_index + self.seq_num_chunks))
 
-        # excise the last token, except for last token of last chunk
-        seq_tokens = np.concatenate((chunks[:, :-1].flatten(), chunks[-1, -1:]))
+            chunks = chunks_memmap[chunk_range]
 
-        # derive retrieved tokens
-        knns = self.knns[chunk_range]
-        continuations = self.continuations[knns]
+            # excise the last token, except for last token of last chunk
+            seq_tokens = np.concatenate((chunks[:, :-1].flatten(), chunks[-1, -1:]))
 
-        # get neighbor and continuation chunks
-        knn_chunks = self.chunks[knns]
-        knn_masks = self.masks[knns]
+            # derive retrieved tokens
+            knns = knns_memmap[chunk_range]
+            continuations = continuations_memmap[knns]
 
-        continuation_chunks = self.chunks[continuations]
-        continuation_masks = self.masks[continuations]
+            # get neighbor and continuation chunks
+            knn_chunks = chunks_memmap[knns]
+            knn_masks = masks_memmap[knns]
 
-        # combine neighbors with continuations
+            continuation_chunks = chunks_memmap[continuations]
+            continuation_masks = masks_memmap[continuations]
 
-        retrieved = np.concatenate((knn_chunks[..., :-1], continuation_chunks[..., :-1]), axis = -1)
-        retrieved_masks = np.concatenate((knn_masks[..., :-1], continuation_masks[..., :-1]), axis = -1)
+            # combine neighbors with continuations
+
+            retrieved = np.concatenate((knn_chunks[..., :-1], continuation_chunks[..., :-1]), axis = -1)
+            retrieved_masks = np.concatenate((knn_masks[..., :-1], continuation_masks[..., :-1]), axis = -1)
 
         return torch.from_numpy(seq_tokens).long(), torch.from_numpy(retrieved).long(), torch.from_numpy(retrieved_masks)
