9f25dc771533edcd36a989fc722060c1a08171ce
Phil Wang <lucidrains@gmail.com>
2022-01-23 16:31:02 -0800

    0.0.26

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'retro-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.0.25',
+  version = '0.0.26',
   license='MIT',
   description = 'RETRO - Retrieval Enhanced Transformer - Pytorch',
   author = 'Phil Wang',
