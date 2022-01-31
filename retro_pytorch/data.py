88096da2bfe7cfe7523edb5ad315e8fe9bc3fed5
Phil Wang <lucidrains@gmail.com>
2022-01-31 15:42:18 -0800

    more prep for inference

--- retro_pytorch/data.py
+++ retro_pytorch/data.py
@@ -6,6 +6,47 @@ from torch.utils.data import Dataset
 from retro_pytorch.retrieval import EOS_ID
 from retro_pytorch.utils import memmap
 
+# knn to retrieved chunks
+
+def knn_to_retrieved_chunks(
+    knns,
+    chunks_memmap,
+    *,
+    add_continuations,
+    num_chunks,
+    pad_id = 0,
+    eos_id = EOS_ID,
+):
+
+    # derive mask for no neighbors found (-1)
+
+    no_neighbor_mask = knns == -1
+    knns = np.maximum(knns, 0)
+
+    # get neighbor and continuation chunks
+
+    knn_chunks = chunks_memmap[knns]
+    is_last_document_chunk = np.any(knn_chunks == eos_id, axis = -1, keepdims = True)
+
+    # use presence of [EOS] in chunk as way to detect document boundaries
+    # [EOS] in BERT tokenizer is 102
+
+    retrieved = knn_chunks[..., :-1]
+
+    if add_continuations:
+        continuation_indices = np.clip(knns + 1, 0, num_chunks - 1) # chunks are stored contiguously
+        continuation_chunks = chunks_memmap[continuation_indices][..., :-1]
+        continuation_chunks *= ~is_last_document_chunk
+
+        # combine neighbors with continuations
+
+        retrieved = np.concatenate((retrieved, continuation_chunks), axis = -1)
+
+    # mask out any nearest neighbor chunks that was -1 (not found at index time) to padding id
+
+    retrieved = np.where(~no_neighbor_mask[..., None], retrieved, pad_id)
+    return retrieved
+
 # dataset
 
 class RETRODataset(Dataset):
@@ -65,33 +106,13 @@ class RETRODataset(Dataset):
 
             knns = knns_memmap[chunk_range]
 
-            # derive mask for no neighbors found (-1)
-
-            no_neighbor_mask = knns == -1
-            knns = np.maximum(knns, 0)
-
-            # get neighbor and continuation chunks
-
-            knn_chunks = chunks_memmap[knns]
-            is_last_document_chunk = np.any(knn_chunks == self.eos_id, axis = -1, keepdims = True)
-
-            # use presence of [EOS] in chunk as way to detect document boundaries
-            # [EOS] in BERT tokenizer is 102
-
-            retrieved = knn_chunks[..., :-1]
-
-            if self.add_continuations:
-                continuation_indices = np.clip(knns + 1, 0, self.num_chunks - 1) # chunks are stored contiguously
-                continuation_chunks = chunks_memmap[continuation_indices][..., :-1]
-                continuation_chunks *= ~is_last_document_chunk
-
-                # combine neighbors with continuations
-
-                retrieved = np.concatenate((retrieved, continuation_chunks), axis = -1)
-
-            # mask out any nearest neighbor chunks that was -1 (not found at index time) to padding id
-
-            retrieved = np.where(~no_neighbor_mask[..., None], retrieved, self.pad_id)
+            retrieved = knn_to_retrieved_chunks(
+                knns,
+                chunks_memmap,
+                add_continuations = self.add_continuations,
+                eos_id = self.eos_id,
+                num_chunks = self.num_chunks
+            )
 
         seq_tokens_torch = torch.from_numpy(seq_tokens).long()
         retrieved_torch = torch.from_numpy(retrieved).long()
